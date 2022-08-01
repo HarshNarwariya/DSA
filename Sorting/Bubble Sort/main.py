59af6a4590a5c26d14cbe10702eaa1269a73eba2
Harsh Narwariya <61902900+HarshNarwariya@users.noreply.github.com>
2022-08-01 17:52:37 +0530

    Delete Sorting directory

--- Sorting/Bubble Sort/main.py
+++
@@ -1,8 +0,0 @@
-array = [1, 2, 5, 4, 3, 1, 2, 8]
-
-for i in range(len(array)):
-    for j in range(len(array) - i - 1):
-        if array[j] > array[j + 1]:
-            array[j], array[j + 1] = array[j + 1], array[j]
-
-print(array)
