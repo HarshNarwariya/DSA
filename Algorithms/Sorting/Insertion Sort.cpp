0640dbd5693e4da1fc8ec130205e8d482db85fbb
Harsh Narwariya <61902900+HarshNarwariya@users.noreply.github.com>
2022-08-02 19:11:26 +0530

    Update Insertion Sort.cpp

--- Algorithms/Sorting/Insertion Sort.cpp
+++ Algorithms/Sorting/Insertion Sort.cpp
@@ -5,7 +5,7 @@ void insertionSort(vector<int> &arr, int start, int end) {
     for (int i = start + 1; i < end; i++) {
         int j = i - 1;
         int temp = arr[i];
-        while (j>=0 && arr[j] > temp) {
+        while (j>=start && arr[j] > temp) {
             arr[j + 1] = arr[j];
             j--;
         }
