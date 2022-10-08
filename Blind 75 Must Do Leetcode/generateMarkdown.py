a694e7cf0b4e5478fe0c2a0b01697fa3c3082ae2
harsh <harsh.narwariya11@gmail.com>
2022-10-08 20:37:14 +0530

    AUTOMATED

--- Blind 75 Must Do Leetcode/generateMarkdown.py
+++ Blind 75 Must Do Leetcode/generateMarkdown.py
@@ -1,6 +1,110 @@
+# print('Loading Data...')
+
+# import os
+# os.chdir(os.path.dirname(__file__))
+# print(os.getcwd())
+
+# import pandas as pd
+# from bs4 import BeautifulSoup
+# import numpy as np
+# import requests
+# import json
+
+
+# # data = requests.get("https://raw.githubusercontent.com/HarshNarwariya/DSA/main/Blind%2075%20Must%20Do%20Leetcode/Leetcode%2075%20Problems.json").json()
+# with open('Leetcode 75 Problems.json') as file:
+#     data = json.load(file)
+# markdown = ""
+
+
+# print('Processing')
+# Difficultys = []
+# Dislikes = []
+# Likes = []
+# Questions = []
+# Solutions = []
+# IsPaidOnlys = []
+# Problems = []
+# Statuss = []
+# Tags = []
+# for item in data:
+#     q = item['Question']
+#     text = q.split('/')[-1]
+#     title = text
+#     text = ''.join(map(lambda t: t[0].upper(), text.split('-')))
+#     question = f"<a href={q}>{text}</a>"
+#     if (item['Solution'] == "#"):
+#         solution = "`NOT DONE`"
+#     else:
+#         title = "%20".join(map(lambda word: word.title(), title.split('-')))
+#         solution = f"<a href=https://github.com/HarshNarwariya/DSA/tree/master/Blind%2075%20Must%20Do%20Leetcode/{title}>{text}-SOL</a>"
+#     Questions.append(question)
+#     Solutions.append(solution)
+#     Difficultys.append(item['Difficulty'])
+#     Dislikes.append(item['Dislikes'])
+#     Likes.append(item['Like'])
+#     IsPaidOnlys.append(item['IsPaidOnly'])
+#     Problems.append(item['Problem'])
+#     if (item['Status'] == True):
+#         Statuss.append(f"`DONE`")
+#     else:
+#         Statuss.append(f"--")
+#     Tags.append(item['Tag'])
+
+# df = pd.DataFrame({
+#     "Title": Problems,
+#     "Status": Statuss,
+#     "Question": Questions,
+#     "Solution": Solutions,
+#     "Difficulty": Difficultys,
+#     "Tag": Tags,
+#     "Like": Likes,
+#     "Dislike": Dislikes,
+# })
+
+
+# whole_table = df.to_markdown()
+# tag_wise_done = df[df['Status'] == '`DONE`']['Tag'].value_counts().to_markdown()
+# diff_wise_done = df[df['Status'] == '`DONE`']['Difficulty'].value_counts().to_markdown()
+# total_status = df['Status'].value_counts().to_markdown()
+# total_tags = df['Tag'].value_counts().to_markdown()
+# total_diffs = df['Difficulty'].value_counts().to_markdown()
+
+
+# markdown =  f'''
+# # Blind 75 Must Do Leetcode
+
+# {whole_table}
+
+# ## Problems Done Yet
+# {total_status}
+
+# ### Total Questions Done Acc To Tags & Difficuties
+
+# {tag_wise_done}
+
+
+
+# {diff_wise_done}
+
+# ### Total Tags & Difficuties
+# {total_tags}
+
+
+
+# {total_diffs}
+# '''
+# print('Writing...')
+# with open('README.md', 'w') as file:
+#     file.write(markdown)
+
+# print('Done!')
+
 print('Loading Data...')
 
 import os
+
+from sqlalchemy import true
 os.chdir(os.path.dirname(__file__))
 print(os.getcwd())
 
@@ -27,6 +131,7 @@ IsPaidOnlys = []
 Problems = []
 Statuss = []
 Tags = []
+DFF = []
 for item in data:
     q = item['Question']
     text = q.split('/')[-1]
@@ -41,6 +146,16 @@ for item in data:
     Questions.append(question)
     Solutions.append(solution)
     Difficultys.append(item['Difficulty'])
+    dff = item['Difficulty']
+    if (dff == 'Easy'):
+        DFFVAL = 0
+    elif (dff == 'Medium'):
+        DFFVAL = 1
+    elif (dff == 'Hard'):
+        DFFVAL = 2
+    else:
+        DFFVAL = 3
+    DFF.append(DFFVAL)
     Dislikes.append(item['Dislikes'])
     Likes.append(item['Like'])
     IsPaidOnlys.append(item['IsPaidOnly'])
@@ -60,7 +175,8 @@ df = pd.DataFrame({
     "Tag": Tags,
     "Like": Likes,
     "Dislike": Dislikes,
-}).sort_values(by=["Difficulty", "Title", "Tag", "Like"])
+    "DFF": DFF,
+}).sort_values(by=['DFF', 'Title', 'Tag'], ignore_index=True)
 
 
 whole_table = df.to_markdown()
